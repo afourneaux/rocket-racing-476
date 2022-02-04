2e9525dc86b3a22569f0cd29fff31a97c74ad36e
Aether700 <69548363+Aether700@users.noreply.github.com>
2022-02-04 13:25:06 -0500

    Tweaked the race track and changed the AI character back to the sphere which has been properly tweaked for racing given it's size and weight

--- Assets/Scripts/AI/RacingAI.cs
+++ Assets/Scripts/AI/RacingAI.cs
@@ -48,7 +48,7 @@ public class RacingAI : MonoBehaviour
         ///////////////////////////////
 
         Vector3 force = Race();
-        rb.rotation = KinematicLookWhereYouAreGoing();
+        rb.rotation = BasicAI.SteeringLookWhereYouAreGoing(rb.rotation, rb.velocity, rotationSpeed);
         rb.AddForce(force);
     }
 
@@ -89,12 +89,5 @@ public class RacingAI : MonoBehaviour
         Vector3 seekOut = BasicAI.SteeringSeek(rb.position, rb.velocity, seekTarget,
             maxAcceleration, maxVelocity, Time.fixedDeltaTime);
         return BasicAI.VelocityToForce(seekOut, rb, Time.fixedDeltaTime);
-    }
-
-    Quaternion KinematicLookWhereYouAreGoing()
-    {
-        if (rb.velocity == Vector3.zero)
-            return transform.rotation;
-        return Quaternion.LookRotation(rb.velocity);
     }
 }
