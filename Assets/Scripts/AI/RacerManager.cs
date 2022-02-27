001e8bfbd00bc25eb0a72e3f454b449531e519d9
Aether700 <69548363+Aether700@users.noreply.github.com>
2022-02-27 12:39:17 -0500

    Implemented Position tracking

--- Assets/Scripts/AI/RacerManager.cs
+++ Assets/Scripts/AI/RacerManager.cs
@@ -7,14 +7,63 @@ public class RacerManager : MonoBehaviour
     private static RacerManager Instance;
 
     private List<Rigidbody> racerRigidbodies = new List<Rigidbody>();
+    private List<PositionTracker> positionTrackers = new List<PositionTracker>();
 
     private void Awake()
     {
         Instance = this;
-        racerRigidbodies.Clear(); // Clear previous information
+        
+        // Clear previous information
+        racerRigidbodies.Clear(); 
+        positionTrackers.Clear();
+    }
+
+    private void Update()
+    {
+        UpdatePositions();
     }
 
     public static void AddRacer(Rigidbody rb) { Instance.racerRigidbodies.Add(rb); }
 
+    public static void AddTracker(PositionTracker tracker) { Instance.positionTrackers.Add(tracker); }
+
     public static List<Rigidbody> GetRacers() { return Instance.racerRigidbodies; }
+
+    private void UpdatePositions()
+    {
+        List<PositionTracker> positionList = new List<PositionTracker>();
+        foreach (PositionTracker tracker in positionTrackers)
+        {
+            tracker.UpdateTrackIndex();
+            AddRacerToPositionList(positionList, tracker);
+        }
+
+        for (int i = 0; i < positionList.Count; i++)
+        {
+            positionList[i].SetPosition(positionList.Count - i);
+        }
+    }
+
+    private void AddRacerToPositionList(List<PositionTracker> positionList, PositionTracker racerToAdd)
+    {
+        for (int i = 0; i < positionList.Count; i++)
+        {
+            if (racerToAdd.GetTrackIndex() == positionList[i].GetTrackIndex())
+            {
+                Vector3 targetPathNode = RaceTrack.GetPathPositions()[racerToAdd.GetTrackIndex()];
+                if (Vector3.Distance(racerToAdd.transform.position, targetPathNode) 
+                    > Vector3.Distance(positionList[i].transform.position, targetPathNode))
+                {
+                    positionList.Insert(i, racerToAdd);
+                    return;
+                }
+            }
+            else if (racerToAdd.GetTrackIndex() < positionList[i].GetTrackIndex())
+            {
+                positionList.Insert(i, racerToAdd);
+                return;
+            }
+        }
+        positionList.Add(racerToAdd);
+    }
 }
