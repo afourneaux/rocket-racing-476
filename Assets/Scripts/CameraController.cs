d4d32ee6d42660f474f20a6aeca4b95302ca24d4
afourneaux <alex@fourneaux.ca>
2022-03-02 03:39:28 -0500

    Camera now turns smoothly about the desired axis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -33,12 +33,11 @@ public class CameraController : MonoBehaviour
         Camera.main.transform.Rotate(new Vector3(0,1,0), horizontal * cameraSpeed * dt);
         Camera.main.transform.Rotate(new Vector3(1,0,0), vertical * cameraSpeed * dt);
 
-        float angleBetween = Vector3.SignedAngle(Camera.main.transform.forward, transform.up, Vector3.Cross(Camera.main.transform.forward, transform.up));
+        float angleBetween = Vector3.SignedAngle(Camera.main.transform.forward, transform.up, Vector3.Cross(transform.up, Camera.main.transform.forward));
 
         if (Mathf.Abs(angleBetween) > cameraDeadAngle) {
-            Vector3 cameraForward = Camera.main.transform.forward;
-            Vector3 rt = (Vector3.RotateTowards(cameraForward, transform.up, cameraDriftSpeed * (angleBetween - cameraDeadAngle) * (rb.velocity.magnitude / 100) * dt, 0.0f));
-            Camera.main.transform.rotation = Quaternion.LookRotation(rt);
+            float angleToMove = angleBetween * Time.deltaTime * cameraDriftSpeed * (rb.velocity.magnitude / 10) * (Mathf.Deg2Rad * (Mathf.Abs(angleBetween) - cameraDeadAngle));
+            Camera.main.transform.RotateAround(Camera.main.transform.position, Vector3.Cross(transform.up, Camera.main.transform.forward), angleToMove);
         }
 
         Camera.main.transform.position = transform.position - (Camera.main.transform.forward * cameraDistance);
