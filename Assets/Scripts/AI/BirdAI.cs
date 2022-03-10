ba7305e32aab8bdd4ef553a25628fad6a986493d
Wendy <71946736+WendyFu004@users.noreply.github.com>
2022-03-10 11:53:08 -0500

    Rework bird AI
    Assign each bird a list of racers inside the restricted area and the area center.
    If the distance between bird and racer is less then the BireRetargetRadius the target will change and also add an collision detection to the bird.

--- Assets/Scripts/AI/BirdAI.cs
+++ Assets/Scripts/AI/BirdAI.cs
@@ -1,19 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class BirdAI : MonoBehaviour
 {
     [SerializeField]
-    private float maxAcceleration = 20.0f;
+    private float maxAcceleration = 200.0f;
     [SerializeField]
-    private float maxVelocity = 40.0f;
+    private float maxVelocity = 250.0f;
     [SerializeField]
     private float rotationSpeed = 1.0f;
 
     [SerializeField]
-    private float chaseArrivalRadius = 0.5f;
+    private float chaseArrivalRadius = 1f;
     [SerializeField]
-    private float chaseSlowDownRadius = 1.0f;
+    private float chaseSlowDownRadius =3.0f;
     [SerializeField]
     private float chaseT2t = 0.5f;
 
@@ -28,7 +29,13 @@ public class BirdAI : MonoBehaviour
     private float shortestDistanceToRotate = 1.0f;
 
     [SerializeField]
-    private float targetRadius = 100.0f;
+    private float RestrictedAreaRadius = 200f;//size of restriced area
+    [SerializeField]
+    private float BireRetargetRadius = 3f;//distance where need to change target
+    [SerializeField]
+    private Transform CenterLocation;
+    [SerializeField]
+    private List<GameObject> Racers;
 
 
 
@@ -36,9 +43,11 @@ public class BirdAI : MonoBehaviour
     private Quaternion startingRotation;
     private Rigidbody rb;
 
+    private List<GameObject> targetlist = new List<GameObject>();
     private Vector3 target;
     private bool resetting = true;
-
+    private bool retarget = true;
+    private int targetIndex =0;
 
     private void Start()
     {
@@ -49,19 +58,30 @@ public class BirdAI : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //search the racers inside the restricted area 
+        RacersInside();
 
-        //find closest AI in the targetRadius otherwise reset
-        if (Vector3.Distance(transform.position, FindClosestAI().transform.position) <= targetRadius)
-            SetDestination(FindClosestAI().transform.position);
-        else
-            ResetPosition();
+        //AI bird sould arrive random racer inside the list
+        if (targetlist.Count!=0 )
+        {
+            if (retarget || targetIndex>targetlist.Count-1 || targetlist.Count!=1)
+            {
+                targetIndex = Random.Range(0, targetlist.Count);
+            }
+            GameObject obj = targetlist[targetIndex];
+            SetDestination(obj.transform.position);
+            print("Target so far: "+obj);
+        }
+
+        //if collsion happens then target next racer
+        ResetTarget();
+
 
         if (!resetting)
         {
             rb.velocity = BasicAI.SteeringArrive(rb.position, rb.velocity, target, chaseSlowDownRadius, 
                 chaseArrivalRadius, maxAcceleration, maxVelocity, chaseT2t, Time.fixedDeltaTime);
-
-            
+           
             if (rb.velocity != Vector3.zero && Vector3.Distance(rb.position, target) > shortestDistanceToRotate)
             {
                 rb.rotation = BasicAI.SteeringLookWhereYouAreGoing(rb.rotation, rb.velocity, rotationSpeed);
@@ -73,10 +93,39 @@ public class BirdAI : MonoBehaviour
         }
     }
 
+    private void RacersInside()
+    {
+        targetlist.Clear();
+        foreach ( GameObject race in Racers)
+        {
+            if (Vector3.Distance(race.transform.position, CenterLocation.position) <= RestrictedAreaRadius)
+            {
+                targetlist.Add(race);
+            }
+        }
+    }
+
     public void SetDestination(Vector3 position)
     {
         target = position;
         resetting = false;
+        retarget = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "AI")
+        {
+            print("*****Collision");
+            retarget = true;
+        }
+    }
+    private void ResetTarget()
+    {
+        if (Vector3.Distance(transform.position, target) < BireRetargetRadius)
+        {
+            print("*****ResetTarget");
+            retarget = true;
+        }
     }
 
     public void ResetPosition()
