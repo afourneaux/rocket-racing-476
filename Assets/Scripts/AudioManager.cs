21ee4daf177bc176a08208ac13f748157cd251e0
linchen2508 <tuanlam80@gmail.com>
2022-04-17 22:47:35 -0400

    Add volume slider
    Add volume slider for music and sound effect ( used text mesh pro so there are many stuff imported)

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,6 +6,7 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
     public Sound[] sounds;
+    public AudioMixer audioMixer;
     void Awake()
     {
         if (Instance == null)
@@ -25,6 +26,7 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.output;
         }
     }
 
@@ -38,4 +40,15 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
     }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSoundEffectVolume(float volume)
+    {
+        audioMixer.SetFloat("soundEffectVolume", volume);
+    }    
+
 }
